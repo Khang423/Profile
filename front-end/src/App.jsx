c9dfb014fe90b9de06bcaffd37835ada15ddfe7b
Vĩ Khang <142973230+Khang423@users.noreply.github.com>
2025-08-26 00:30:05 +0700

    update (#1)
    Co-authored-by: Azen <vovykhag@gmail.com>

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
-const App = () => {
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMsg(data.message));
-  }, []);
-
-  return <div className="text-3xl underline">{msg}</div>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen w-full ">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="p-4 flex-1">This is the main content</main>;
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ front-end/src/components/Sidebar.jsx
@@ -0,0 +1,4 @@
+const Sidebar = () => {
+  return <aside className="bg-gray-100 p-4 w-1/4">Menu Sidebar</aside>;
+};
+export default Sidebar;
