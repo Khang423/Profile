b04439e5f9c34109cc9424ece8e8c407c5854bef
Azen <vovykhag@gmail.com>
2025-08-26 00:28:31 +0700

    update

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">My Website</h1>
+        <h1 className="text-xl font-medium"> Right content</h1>
+      </div>
+    </header>
+  );
+};
+export default Header;
