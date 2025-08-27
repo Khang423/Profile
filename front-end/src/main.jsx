21301e81e9e6ac9c6c61b78d2874d186608539d7
Vĩ Khang <142973230+Khang423@users.noreply.github.com>
2025-08-27 17:10:19 +0700

    Dev (#3)
    * update
    
    * update
    
    * update code
    
    ---------
    
    Co-authored-by: Azen <vovykhag@gmail.com>

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TestApi from './Test.jsx';
+import UserList from './UserList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <TestApi />
+    <UserList />
   </StrictMode>,
 )
