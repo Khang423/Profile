dd9446fabb0875635a6f936d5292bd79af8acee0
Azen <vovykhag@gmail.com>
2025-08-30 22:34:30 +0700

    fix conflict

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AdminLayout from "../layouts/AdminLayout";
 const Dashboard = () => {
   return (
@@ -7,22 +6,6 @@ const Dashboard = () => {
         <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
       </div>
     </AdminLayout>
-=======
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
-import Footer from "../components/Footer";
-
-const Dashboard = () => {
-  return (
-    <div className="flex flex-col min-h-screen w-full ">
-      <Header />
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="p-4 flex-1">This is the main content</main>;
-      </div>
-      <Footer />
-    </div>
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
   );
 };
 export default Dashboard;
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,28 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/authApi";
-<<<<<<< HEAD
 import { message } from "antd";
-=======
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-<<<<<<< HEAD
   const [errors, setErrors] = useState({});
-=======
-  const [error, setError] = useState("");
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-<<<<<<< HEAD
-=======
-    setError("");
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
     setLoading(true);
 
     try {
@@ -30,7 +19,6 @@ const Login = () => {
       localStorage.setItem("token", res.data.token);
       navigate("/admin/dashboard");
     } catch (err) {
-<<<<<<< HEAD
       console.log();
       if (err.response?.data?.errors) {
         const fieldErrors = {};
@@ -41,10 +29,6 @@ const Login = () => {
       } else if (err.response?.data?.message) {
         setErrors({ message: err.response?.data?.message });
       }
-=======
-      console.error(err);
-      setError(err.response?.data?.message || "Đăng nhập thất bại");
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
     } finally {
       setLoading(false);
     }
@@ -53,15 +37,6 @@ const Login = () => {
     <div className="flex h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-sm rounded-2xl bg-white p-6 shadow-md">
         <h1 className="mb-4 text-2xl font-bold text-center">Đăng nhập</h1>
-<<<<<<< HEAD
-
-=======
-        {error && (
-          <div className="mb-3 rounded bg-red-100 p-2 text-sm text-red-600">
-            {error}
-          </div>
-        )}
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
             type="text"
@@ -70,10 +45,7 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
             className="w-full rounded-md border p-2"
           />
-<<<<<<< HEAD
           {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
-=======
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
           <input
             type="password"
             placeholder="Mật khẩu"
@@ -81,11 +53,8 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
             className="w-full rounded-md border p-2"
           />
-<<<<<<< HEAD
           {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
           {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
-=======
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
           <button
             type="submit"
             disabled={loading}
