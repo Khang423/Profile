1035cf15603ad9dc18eac5a62a2c69903b42aabe
Vĩ Khang <142973230+Khang423@users.noreply.github.com>
2025-08-30 22:41:46 +0700

    Dev (#6)
    * update
    
    * update
    
    * update code
    
    * login
    
    * feat: login , status:done
    
    * fix conflict
    
    ---------
    
    Co-authored-by: Azen <vovykhag@gmail.com>

--- back-end/src/routes/authRoutes.js
+++ back-end/src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
-<<<<<<< HEAD
 const validateMiddleware = require("../middleware/validateMiddleware");
 const loginValidation = require("../validation/loginValidation");
 
@@ -11,8 +10,5 @@ router.post(
   validateMiddleware,
   authController.login
 );
-=======
 
-router.post("/login", authController.login);
->>>>>>> b7a04dc72c2ca660008e76bd6a506ee416e6c412
 module.exports = router;
